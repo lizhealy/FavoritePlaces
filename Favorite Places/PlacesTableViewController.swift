2209939d8c417e35ba4491149bf192e0fa2c1ed9
lizhealy <lizhealy8@gmail.com>
2017-01-24 12:39:35 -0500

    Initial Commit

---
+++ Favorite Places/PlacesTableViewController.swift
@@ -0,0 +1,121 @@
+//
+//  PlacesTableViewController.swift
+//  Favorite Places
+//
+//  Created by Liz Healy on 1/24/17.
+//  Copyright © 2017 netGalaxy Studios. All rights reserved.
+//
+
+import UIKit
+
+var places = [Dictionary<String, String>()]
+var activePlace = -1
+
+class PlacesTableViewController: UITableViewController {
+
+    @IBOutlet var table: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if let tempPlaces = UserDefaults.standard.object(forKey: "places") as? [Dictionary<String, String>] {
+            places = tempPlaces
+        }
+        
+        if places.count == 1 && places[0].count == 0 {
+            places.remove(at: 0)
+            places.append(["name":"Taj Mahal", "lat":"27.175277", "lon":"78.042128"])
+            
+            UserDefaults.standard.set(places, forKey: "places")
+            
+        }
+        
+        activePlace = -1
+        table.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return places.count
+        
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        if places[indexPath.row]["name"] != nil {
+            cell.textLabel?.text = places[indexPath.row]["name"]
+        }
+
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        activePlace = indexPath.row
+        
+        performSegue(withIdentifier: "toMap", sender: nil)
+    }
+    
+
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
+        return true
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            places.remove(at: indexPath.row)
+            
+            UserDefaults.standard.set(places, forKey: "places")
+            
+            table.reloadData()
+        }    
+    }
+
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Favorite Places/ViewController.swift
+++ Favorite Places/ViewController.swift
@@ -7,19 +7,123 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let lpg = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress (gestureRecognizer:)))
+        lpg.minimumPressDuration = 2
+        map.addGestureRecognizer(lpg)
+        
+        if activePlace == -1 {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+        else {
+        
+            if places.count > activePlace {
+                if let name = places[activePlace]["name"] {
+                    if let lat = places[activePlace]["lat"] {
+                        if let lon = places[activePlace]["lon"] {
+                            if let latitude = Double(lat) {
+                                if let longitude = Double(lon) {
+                                    
+                                    let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                    let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                                    let region = MKCoordinateRegion(center: coordinate, span: span)
+                                    
+                                    self.map.setRegion(region, animated: true)
+                                    
+                                    let annotation = MKPointAnnotation()
+                                    
+                                    annotation.coordinate = coordinate
+                                    annotation.title = name
+                                    
+                                    self.map.addAnnotation(annotation)
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    
+    func longpress(gestureRecognizer: UIGestureRecognizer) {
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.began {
+            let touchPoint = gestureRecognizer.location(in: self.map)
+            let newCoordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+            let location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            
+            var title = ""
+            
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) in
+                
+                if error != nil {
+                    print(error)
+                }
+                else {
+                    if let placemark = placemarks?[0] {
+                        if placemark.subThoroughfare != nil {
+                            title += placemark.subThoroughfare! + " "
+                        }
+                        if placemark.thoroughfare != nil {
+                            title += placemark.thoroughfare!
+                        }
+                        
+                    }
+                }
+                
+                if title == "" {
+                    
+                    title = "Added \(NSDate())"
+                    
+                }
+            
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = title
+            annotation.coordinate = newCoordinate
+            
+            self.map.addAnnotation(annotation)
+            places.append(["name":title, "lat":String(newCoordinate.latitude), "lon":String(newCoordinate.longitude)])
+            
+            UserDefaults.standard.set(places, forKey: "places")
+            
+            })
+            
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.map.setRegion(region, animated: true)
+        
+        
+    }
+    
 }
 
